eee582ba52535a614f5e97c6ad202565f82c4ff5
Joyce Yeung <joyce.yeung@myob.com>
2020-09-21 15:00:07 +1000

    completed door class

---
+++ MontyHall/Door.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace MontyHall
+{
+    public class Door
+    {
+       
+        public bool HasPrize { get; private set; }
+
+        public Door()
+        {
+            HasPrize = false;
+        }
+
+        public bool Reveal()
+        {
+            return this.HasPrize;
+        }
+
+        public void AssignPrize()
+        {
+            this.HasPrize = true;
+        }
+        
+    }
+}
